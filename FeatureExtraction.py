3492ae77ea7a270ca977b730edcd7a3105cdce46
wendiganyu <jinyang.li.21@ucl.ac.uk>
2021-11-14 16:46:05 +0000

    Add PCA feature extraction method.

---
+++ FeatureExtraction.py
@@ -0,0 +1,32 @@
+from sklearn.decomposition import PCA
+
+def PCAFeatureExtraction(X, k):
+    """
+    Do the Principle Component Analysis (PCA) method to original input data
+    to get the dimensional reduced output, which is smaller and reduces disturbance
+    of outliers, with the help of sklearn functions.
+
+    Inputs
+        X: dataset;
+        k: number of components.
+
+    Return
+        pca_data: reduced dataset after PCA. This data varies with the changing of input k value.
+        SValue: The singular values corresponding to each of the selected components.
+        Variance: The amount of variance explained by each of the selected components.
+                It will provide you with the amount of information or variance each principal component holds after projecting the data to a lower dimensional subspace.
+        Vcomp: The estimated number of components.
+    """
+
+    # Built-in function for PCA,
+    # where n_clusters is the number of clusters.
+    pca = PCA(n_components=k)
+
+    # Fit the algorithm with dataset
+    pca.fit(X)
+
+    pca_data = pca.transform(X)
+    SValue = pca.singular_values_
+    Variance = pca.explained_variance_ratio_
+    Vcomp = pca.components_
+    return pca_data, SValue, Variance, Vcomp
--- PreProcessing.py
+++ PreProcessing.py
@@ -1,17 +1,15 @@
-"""
-Transform the raw MRI dataset files to the formats we use in processing the programs.
-For example, matrix, pandas dataframes, etc.
-"""
 import os
 import cv2
 import pandas as pd
 import numpy as np
-import matplotlib as plt
 
 
-def gen_mri_mtx_binary(data_dir, label_path):
+def gen_mri_mtx_binary_label(data_dir, label_path):
     """
-    Convert the 3000 MRI dataset .ipg files into a big matrix
+    Transform the 3000 raw .jpg images in MRI dataset combined with
+    their binary label data indicating the existence of brain tumor or not
+    together to a big numpy matrix we could use in program processing.
+
     Input
         dir：location of the dataset directory
     Return
@@ -21,12 +19,11 @@ def gen_mri_mtx_binary(data_dir, label_path):
                  is its class (0:no_tumor, 1:with_tumor).
     """
 
-    # mri_mtx = []
-    mri_mtx = np.empty((0,262145), int)
+    mri_mtx = np.empty((0, 262145), int)
     df = pd.read_csv(label_path)
     df = df.set_index('file_name')
 
-    for fileName in os.listdir(data_dir):
+    for fileName in sorted(os.listdir(data_dir)):
         print("This is img " + fileName)
         # Read an image as a matrix.
         full_path = os.path.join(data_dir, fileName)
@@ -44,12 +41,18 @@ def gen_mri_mtx_binary(data_dir, label_path):
 
         # Append this row data vector to mri_matrix.
         mri_mtx = np.append(mri_mtx, [data_vec], axis=0)
-        # mri_mtx.append([data_vec])
     return mri_mtx
 
 
-data_dir = "dataset/image"
-label_path = "dataset/label.csv"
-mri_mtx = gen_mri_mtx_binary(data_dir, label_path)
-print(mri_mtx)
-np.savetxt("MRI_Matrix.txt", mri_mtx, fmt="%d", delimiter=",")
+if __name__ == "__main__":
+    data_dir = "dataset/image"
+    label_path = "dataset/label.csv"
+    mtx_file_name = "MRI_Matrix.npy"
+
+    # Check if the data matrix already saved as file.
+    if os.path.exists(mtx_file_name):
+        mri_mtx = np.load(mtx_file_name)
+    else:
+        mri_mtx = gen_mri_mtx_binary_label(data_dir, label_path)
+        np.save(mtx_file_name, mri_mtx)
+    print(mri_mtx.shape)
